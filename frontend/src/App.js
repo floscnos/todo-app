c56c2ebc99ce9f089e448d4a51b7a1daa247fce8
floscnos <floris@floriscnossen.nl>
2022-04-18 17:10:49 +0200

    start at frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,75 @@
+import React, { Component } from "react";
+
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const todoItems = [
+  {
+    id: 1,
+    title: "Go to Market",
+    description: "Buy ingredients to prepare dinner",
+    completed: true,
+  },
+  {
+    id: 2,
+    title: "Study",
+    description: "Read Algebra and History textbook for the upcoming test",
+    completed: false,
+  },
+  {
+    id: 3,
+    title: "Sammy's books",
+    description: "Go to library to return Sammy's books",
+    completed: true,
+  },
+  {
+    id: 4,
+    title: "Article",
+    description: "Write article on how to use Django with React",
+    completed: false,
+  },
+];
+
+class App extends Component {
+
+  constructor(props) {
+    super();
+    this.state = {
+      viewCompleted: false,
+      todoList: todoItems,
+    }
+  }
+
+  displayCompleted =  (status) => {
+    if (status) {
+      return this.setState({ viewCompleted: true});
+    }
+
+    return this.setState({ viewCompleted: false})
+  }
+
+  renderTabList = () => {
+    return (
+        <div className="nav nav-tabs">
+          <span
+              className={this.state.viewCompleted ? "nav-line active" : "nav-link"}
+              onClick={() => this.displayCompleted(true)}
+            >
+            Complete
+          </span>
+          <span
+              className={this.state.viewCompleted ? "nav-line active" : "nav-link"}
+              onClick={() => this.displayCompleted(true)}
+          >
+            Incomplete
+          </span>
+        </div>
+    );
+  };
+
+  renderItems =
+
+
 }
 
 export default App;
